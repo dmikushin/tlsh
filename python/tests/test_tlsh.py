04a48d291db84effa5aa10192073394b8e653448
Christophe Alladoum <christophe.alladoum@elastic.co>
2024-01-30 11:29:30 -0800

    restored base tests

--- python/tests/test_tlsh.py
+++ python/tests/test_tlsh.py
@@ -4,7 +4,7 @@ import random
 
 CURFILE = pathlib.Path(__file__)
 CURDIR = CURFILE.parent
-TEST_DATA_PATH = CURDIR / "../../tests"
+TEST_DATA_PATH = CURDIR / "../../tests/datasets"
 
 
 def rand(n: int):
@@ -41,11 +41,11 @@ def test_basic_test():
         assert a.diff(b) == 0
 
 
-def test_batch_test():
+def test_batch_test_base():
     tlsh = __import__("tlsh")
     expected_values = [
         x.strip().split(maxsplit=1)
-        for x in (TEST_DATA_PATH / "base_dataset.txt").open().readlines()
+        for x in (TEST_DATA_PATH / "base.txt").open().readlines()
     ]
 
     for fname, expected_value in expected_values:
